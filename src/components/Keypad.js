b25df1fbc1e9354f4bfd1ed8292fe2b5cf9ff7e2
Benard Abwao <benard.okoth.abwao@gmail.com>
2022-07-14 10:54:42 +0300

    Done.

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,13 @@
 // Code Keypad Component Here
+function Keypad(){
+    return (
+      <input 
+        onChange={ () => console.log(`Entering password...`)} 
+        style = {{ width : "250px", height : "50px", buttonColor : "blue"}} 
+        placeholder = "Password"
+        type= "password"
+      />
+    )
+  }
+  
+  export default Keypad;
